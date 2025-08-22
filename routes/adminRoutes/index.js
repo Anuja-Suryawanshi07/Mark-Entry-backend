f13eed585a40189eb25a0516efd8a9e779e6c2f0
Anuja-Suryawanshi07 <anuja.suryawanshi@gmail.com>
2025-08-22 11:08:23 +0530

    anuja- testing done for few routes

--- routes/adminRoutes/index.js
+++ routes/adminRoutes/index.js
@@ -10,7 +10,8 @@ const updateStaff = require("./updateStaff");
 const deletestaff = require("./deleteStaff");
 const getAllStudents = require('./getallstudents');
 const addStudentToGroup = require('./addStudentToGroup');
-
+const dashboard = require ("./dashboard");
+const getAllCourses = require("./getAllCourses");
 
 router.use(getAllStaff);
 router.use(addStaff);
@@ -18,6 +19,8 @@ router.use(updateStaff);
 router.use(deletestaff);
 router.use(getAllStudents);
 router.use(addStudentToGroup);
+router.use(dashboard);
+router.use(getAllCourses);
 
 module.exports = router;
 

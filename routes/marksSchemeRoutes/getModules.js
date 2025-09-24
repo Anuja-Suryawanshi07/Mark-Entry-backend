6bb8549684540ab808668bc7055b2f185c080463
w3-80717 <chavan1609@gmail.com>
2025-09-24 20:27:18 +0530

    use promise mysql2

--- routes/marksSchemeRoutes/getModules.js
+++ routes/marksSchemeRoutes/getModules.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../../config/db");
+const pool = require("../../config/db-promise");
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
 
 // ✅ Get all modules (for dropdown in Add Marks form)

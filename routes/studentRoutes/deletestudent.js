5d9d456b4d609e77a3c0f344bf74dc0d90855be8
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 00:29:01 +0530

    Update deletestudent.js

--- routes/studentRoutes/deletestudent.js
+++ routes/studentRoutes/deletestudent.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const pool = require("../../config/db");
+
 const router = express.Router();
 const { STUDENT_TABLE } = require("../../config"); // Make sure STUDENT_TABLE = 'student' in config
 

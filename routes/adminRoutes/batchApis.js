cf57813807a561e8b5e538878bbf7998584a2cd9
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 00:36:57 +0530

    Update batchApis.js

--- routes/adminRoutes/batchApis.js
+++ routes/adminRoutes/batchApis.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../../config/db");
+
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
-const { BATCH_TABLE } = require("../../config");
+
 
 
 //http://localhost:7777/admin/all-batch

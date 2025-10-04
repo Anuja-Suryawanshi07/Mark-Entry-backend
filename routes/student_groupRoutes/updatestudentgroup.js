e36e7d47eeba6aa06aa8045de2cf4808aa7ecea3
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 01:29:54 +0530

    Update updatestudentgroup.js

--- routes/student_groupRoutes/updatestudentgroup.js
+++ routes/student_groupRoutes/updatestudentgroup.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const pool = require("../../config/db");
+
 const router = express.Router();
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
-const { STUDENT_GROUP_TABLE } = require("../../config");
+
 // PUT: update a student group by group_id
 
 // Example: PUT http://localhost:7777/student-groups/update-group/11

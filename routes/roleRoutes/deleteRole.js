fad6e3927f602041947b1299056e2bc7482a3899
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 00:23:16 +0530

    Update deleteRole.js

--- routes/roleRoutes/deleteRole.js
+++ routes/roleRoutes/deleteRole.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../../config/db");
+
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
 const { ROLE_TABLE } = require("../../config");
 
@@ -34,3 +34,4 @@ router.delete("/delete-role/:roleId", (req, res) => {
 });
 
 module.exports = router;
+

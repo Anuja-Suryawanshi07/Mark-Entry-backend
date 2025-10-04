89b71f639ec9e7c4232274709c09b122d283b05f
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 01:10:49 +0530

    Update updateModule.js

--- routes/moduleRoutes/updateModule.js
+++ routes/moduleRoutes/updateModule.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const pool = require("../../config/db");
+
 const router = express.Router();
-const { MODULE_TABLE } = require("../../config");
+
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
 
 // PUT: update an user by Id

0e13f0e60d4362982b28e9e1360c079b0712a612
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 01:06:09 +0530

    Update getModules.js

--- routes/mentorRoutes/getModules.js
+++ routes/mentorRoutes/getModules.js
@@ -1,6 +1,6 @@
 // routes/mentorRoutes/getModules.js
 const express = require("express");
-const pool = require("../../config/db");
+
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
 
 const router = express.Router();

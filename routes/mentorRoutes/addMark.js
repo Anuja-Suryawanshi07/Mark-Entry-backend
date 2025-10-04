80049688023149c79e0f943f8da5a29fa934097d
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 01:03:39 +0530

    Update addMark.js

--- routes/mentorRoutes/addMark.js
+++ routes/mentorRoutes/addMark.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const pool = require("../../config/db");
+
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
 
 const router = express.Router();

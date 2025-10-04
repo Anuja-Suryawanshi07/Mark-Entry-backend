d7675bdd295dd9c717bd0431ac9c3ac91028670c
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 00:29:27 +0530

    Update getMarksByStudentId.js

--- routes/studentRoutes/getMarksByStudentId.js
+++ routes/studentRoutes/getMarksByStudentId.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const pool = require("../../config/db"); // adjust path if needed
+
 const router = express.Router();
 const { STUDENT_TABLE } = require("../../config");
 

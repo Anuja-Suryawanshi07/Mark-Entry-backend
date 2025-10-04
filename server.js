bddca88e42203fcbe7bc29504657acb439d3ea4d
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-04 22:09:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const { PORT } = require("./config");
 
+
+// Import the DB pool
+const db = require("./config/db");
+
 const app = express();
 app.use(express.json());
 app.use(cors());

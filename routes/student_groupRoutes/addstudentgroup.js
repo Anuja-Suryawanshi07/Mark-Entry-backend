61ad5fab887def1ff3b524d7e0f567b53e7d33fc
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 01:26:44 +0530

    Update addstudentgroup.js

--- routes/student_groupRoutes/addstudentgroup.js
+++ routes/student_groupRoutes/addstudentgroup.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../../config/db');
+
 const { successResponse, errorResponse } = require('../../utils/apiResponse');
-const { STUDENT_GROUP_TABLE } = require("../../config");
+
 
 // Your existing route here:
 

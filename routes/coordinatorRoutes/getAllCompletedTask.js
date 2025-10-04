6768ed3889b25bae4e9a8e5520570f05545e4937
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 00:47:55 +0530

    Update getAllCompletedTask.js

--- routes/coordinatorRoutes/getAllCompletedTask.js
+++ routes/coordinatorRoutes/getAllCompletedTask.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const pool = require("../../config/db");
+
 const router = express.Router();
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
 
@@ -42,5 +42,6 @@ router.get("/completed-tasks", (req, res) => {
       .json(successResponse(result, "Approved tasks fetched successfully"));
   });
 });
+
 
 module.exports = router;

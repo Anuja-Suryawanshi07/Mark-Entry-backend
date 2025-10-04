11c772d0e3eca04ec300174197250e2e1966c16c
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 01:14:30 +0530

    Update assignStaffToCourse.js

--- routes/staffRoutes/assignStaffToCourse.js
+++ routes/staffRoutes/assignStaffToCourse.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
-const { STAFF_TABLE, COURSE_TABLE } = require("../../config");
+
 
 /*
  * PUT: Assign / Update Staff Course

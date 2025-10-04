81dc70d9a97d55f5ec297e34533c5d4673eb4135
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 01:15:03 +0530

    Update deletestaff.js

--- routes/staffRoutes/deletestaff.js
+++ routes/staffRoutes/deletestaff.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
-const { STAFF_TABLE } = require("../../config");
+
 
 // DELETE: delete an staff
 //http://localhost:7777/staff/delete-staff/6

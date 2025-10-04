46f37dff3db1741624c36e59dc2734c4a425d905
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 00:33:21 +0530

    Update ModuleApis.js

--- routes/adminRoutes/ModuleApis.js
+++ routes/adminRoutes/ModuleApis.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const pool = require("../../config/db");
+
 const router = express.Router();
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
-const { MODULE_TABLE, COURSE_TABLE } = require("../../config");
+
 
 
 // GET all users

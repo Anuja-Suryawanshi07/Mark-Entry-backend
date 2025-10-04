2c8baa47e134126677e610f4cdf937a659d7294c
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 00:43:23 +0530

    Update updateBatch.js

--- routes/batchRoutes/updateBatch.js
+++ routes/batchRoutes/updateBatch.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../../config/db");
+
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
-const { BATCH_TABLE } = require("../../config");
+
 
 //http://localhost:7777/batch/update-batch/batchId
 
@@ -40,3 +40,4 @@ router.put("/update-batch/:batchId", (req, res) => {
 });
 
 module.exports = router;
+

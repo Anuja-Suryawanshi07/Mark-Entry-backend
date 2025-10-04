c880e0f91a490ae4807f75744997b2fa580dbe7e
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 00:26:55 +0530

    Update staffLogin.js

--- routes/staffRoutes/staffLogin.js
+++ routes/staffRoutes/staffLogin.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const pool = require("../../config/db");
+
 const router = express.Router();
 const crypto = require('crypto-js')
 const jwt = require('jsonwebtoken')

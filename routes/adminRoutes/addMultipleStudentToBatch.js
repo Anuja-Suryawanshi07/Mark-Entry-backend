71410aa26a53eb2e6648626c4048cd2e4c24e3a6
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 00:34:25 +0530

    Update addMultipleStudentToBatch.js

--- routes/adminRoutes/addMultipleStudentToBatch.js
+++ routes/adminRoutes/addMultipleStudentToBatch.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../../config/db"); 
+
 
 //Admin Add MULTIPLE Students to Batch API
 //Endpoint: POST /admin/add-students-to-batch

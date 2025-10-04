796220ba76577896a76c0e3dd5ad30d4d0fdade5
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 01:00:18 +0530

    Update deleteMarks.js

--- routes/marksRoutes/deleteMarks.js
+++ routes/marksRoutes/deleteMarks.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const pool = require("../../config/db");
+
 const router = express.Router();
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
-const { MARKS_TABLE } = require("../../config");
+
 
 // DELETE: delete an Marks
 //http://localhost:7777/marks/delete-marks/6

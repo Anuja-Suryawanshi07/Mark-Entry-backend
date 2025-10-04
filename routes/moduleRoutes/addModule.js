467a49926e5bda155e20dc6f355ecf9548f8c4f8
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 01:08:24 +0530

    Update addModule.js

--- routes/moduleRoutes/addModule.js
+++ routes/moduleRoutes/addModule.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const pool = require("../../config/db");
 const router = express.Router();
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
-const { MODULE_TABLE } = require("../../config");
+
 
 
 // POST: add new Module in a Specific Course

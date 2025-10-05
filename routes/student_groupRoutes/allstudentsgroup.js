b1c85c5196fe962cdd879cc3f797a5e0168f5818
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 14:13:02 +0530

    Update allstudentsgroup.js

--- routes/student_groupRoutes/allstudentsgroup.js
+++ routes/student_groupRoutes/allstudentsgroup.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
+const pool = require("../../config/db");
 const router = express.Router();
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
-
+const { STUDENT_GROUP_TABLE } = require("../../config");
 // GET all student groups
 
 // http://localhost:7777/student-groups/get-all-groups

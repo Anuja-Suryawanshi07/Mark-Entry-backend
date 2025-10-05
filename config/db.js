d2a02d6801511fdb8e70b92ec7a9777045e40012
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 12:16:51 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,34 +1,19 @@
-// db.js
-const mysql = require('mysql2'); // use promise wrapper
-const fs = require('fs');
-
-
-const HOST = process.env.DB_HOST;
-const USERNAME = process.env.DB_USERNAME ; 
-const PASSWORD = process.env.DB_PASSWORD ;
-const DATABASE = process.env.DB_NAME ;
-const PORT = process.env.PORT || 7777;
-const DB_PORT = process.env.DB_PORT || 4000;
-
-
+const mysql = require("mysql2");
+const {HOST, USERNAME, PASSWORD, DATABASE, DB_PORT} = require(".");
 const pool = mysql.createPool({
   host: HOST,
-  port: DB_PORT,
+  port:DB_PORT,
   user: USERNAME,
   password: PASSWORD,
   database: DATABASE,
-  charset: 'utf8mb4',
   waitForConnections: true,
   connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
   queueLimit: 0,
-
-  ssl: HOST.includes("localhost")?undefined:{ rejectUnauthorized: true } // Add this line for secure connection
-  // ssl: {
-
-    
-  //   //ca: fs.readFileSync(__dirname +'/ca.pem')
-
-  // }
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+    ssl: HOST.includes("localhost")?undefined:{ rejectUnauthorized: true } // Add this line for secure connection
 });
 
 module.exports = pool;

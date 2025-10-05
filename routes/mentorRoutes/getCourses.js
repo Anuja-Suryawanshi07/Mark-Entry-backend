23a6d6bd162a3461c44772ec68676448ded3dd3f
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 13:48:50 +0530

    Update getCourses.js

--- routes/mentorRoutes/getCourses.js
+++ routes/mentorRoutes/getCourses.js
@@ -1,6 +1,6 @@
 // routes/mentorRoutes/getCourses.js
 const express = require("express");
-
+const pool = require("../../config/db");
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
 
 const router = express.Router();

2c1171607559192c1bb3a4fa26f2f9e02530841b
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 13:59:54 +0530

    Update allTasks.js

--- routes/staffRoutes/allTasks.js
+++ routes/staffRoutes/allTasks.js
@@ -1,5 +1,6 @@
-const express = require("express");
 
+const express = require("express");
+const pool = require("../../config/db");
 const router = express.Router();
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
 

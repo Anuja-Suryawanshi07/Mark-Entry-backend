97089fa5169508a10d276e7803056a1d3077ef03
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 14:02:49 +0530

    Update staffLogin.js

--- routes/staffRoutes/staffLogin.js
+++ routes/staffRoutes/staffLogin.js
@@ -1,10 +1,9 @@
 const express = require("express");
-
+const pool = require("../../config/db");
 const router = express.Router();
 const crypto = require('crypto-js')
 const jwt = require('jsonwebtoken')
-const SECRET_KEY = process.env.SECRET_KEY;
-
+const {SECRET_KEY} = require('../../config')
 // POST: Staff Login
 // Example: POST http://localhost:7777/staff/login
 router.post("/login", (req, res) => {

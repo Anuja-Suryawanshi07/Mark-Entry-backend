ced66a34d75d68b35f683b195df3a1195713245e
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 13:54:55 +0530

    Update getModulesByCourseId.js

--- routes/moduleRoutes/getModulesByCourseId.js
+++ routes/moduleRoutes/getModulesByCourseId.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const pool = require("../../config/db");
 const router = express.Router();
-
+const { MODULE_TABLE } = require("../../config");
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
 
 

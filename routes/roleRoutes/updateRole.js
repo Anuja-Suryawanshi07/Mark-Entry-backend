d738cbca5e0ae4efa946c0f64a0f6dc6a4cba6fb
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 13:58:51 +0530

    Update updateRole.js

--- routes/roleRoutes/updateRole.js
+++ routes/roleRoutes/updateRole.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-
+const pool = require("../../config/db");
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
-
+const { ROLE_TABLE } = require("../../config");
 
 //http://localhost:7777/roles/update-role/6
 
@@ -39,5 +39,3 @@ router.put("/update-role/:roleId", (req, res) => {
 });
 
 module.exports = router;
-
-

2bc052ce56522c0585dba741e6438aaab03aa442
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 14:06:52 +0530

    Update getStudentDetails.js

--- routes/studentRoutes/getStudentDetails.js
+++ routes/studentRoutes/getStudentDetails.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
+const pool = require("../../config/db");
 const router = express.Router();
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
-
+const { STUDENT_TABLE, STUDENT_GROUP_TABLE, USER_TABLE, COURSE_TABLE, BATCH_TABLE } = require("../../config");
 
 // GET all students
 // http://localhost:7777/student/get-student-details

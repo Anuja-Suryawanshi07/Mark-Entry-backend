23fc0406b7116d5aeb7335b930fe98f7d1c0586f
Anuja-Suryawanshi07 <165706995+Anuja-Suryawanshi07@users.noreply.github.com>
2025-10-05 13:39:15 +0530

    Update getCourseById.js

--- routes/courseRoutes/getCourseById.js
+++ routes/courseRoutes/getCourseById.js
@@ -1,7 +1,7 @@
 const express = require("express");
-
+const pool = require("../../config/db");
 const router = express.Router();
-
+const { COURSE_TABLE } = require("../../config");
 const { successResponse, errorResponse } = require("../../utils/apiResponse");
 
 
